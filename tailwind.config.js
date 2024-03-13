2c6cb3427bdb18fa389a5c797c5e96346dfc5ad3
shirguin <shirguin@yandex.ru>
2024-03-13 15:41:46 +0300

    not finished second block

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,25 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
+      animation: {
+        pulse_1: "pulse_1 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+      },
+      keyframes: {
+        pulse_1: {
+          "0%, 100%": { opacity: 1 },
+          "50%": { opacity: 0 },
+        },
+      },
       screens: {
         mobile: "360px",
       },
       colors: {
         mainWhite: "#FFFFFF",
         mainGray: "#6C6C6C",
+        secondGray: "#3A3A3A",
         mainBlack: "#000000",
+        secondBlack: "#303030",
+        shadowBlack: "rgba(0, 0, 0, 0.9)",
         mainRed: "#B91C23",
         secondRed: "#530D10",
         currentColor: "#FFFFFF",
