26f9b1daa3d8f72fc220e735cda594a7d2e1b459
shirguin <shirguin@yandex.ru>
2024-03-13 22:05:43 +0300

    made a mobile version of the second block

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,21 @@ module.exports = {
     extend: {
       animation: {
         pulse_1: "pulse_1 1.5s cubic-bezier(0.4, 0, 0.6, 1) infinite",
+        // vibrate: "vibrate 0.2s liner infinity both",
       },
       keyframes: {
         pulse_1: {
           "0%, 100%": { opacity: 1 },
           "50%": { opacity: 0 },
         },
+        // vibrate: {
+        //   "0%": { transform: "translate(0)" },
+        //   "20%": { transform: 'translate("-2px", "2px")' },
+        //   "40%": { transform: 'translate("-2px", "-2px")' },
+        //   "60%": { transform: 'translate("2px", "2px")' },
+        //   "80%": { transform: 'translate("2px", "-2px")' },
+        //   "100%": { transform: "translate(0)" },
+        // },
       },
       screens: {
         mobile: "360px",
